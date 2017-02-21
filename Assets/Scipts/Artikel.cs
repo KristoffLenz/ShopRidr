f74349cd15899cc89f37b8e3584e3872e63db143
llohmann <llohmann@stud.hs-heilbronn.de>
2017-02-21 16:39:04 +0100

    Enum Artikelkategorie ausgelagert

--- Assets/Scipts/Artikel.cs
+++ Assets/Scipts/Artikel.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Artikel : MonoBehaviour {
 
-    public enum artikelKategorie { Gemüse, Obst, Getränke, Backwaren };
+    EnumArtikelkategorie artikelkategorie;
     string name;
     // Use this for initialization
     void Start () {
--- Assets/Scipts/Regal.cs
+++ Assets/Scipts/Regal.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Regal : MonoBehaviour {
-    private enum artikelKategorie { Gemüse, Obst, Getränke, Backwaren };
+
+    EnumArtikelkategorie artikelkategorie;
     private enum ausrichtung { senkrecht_rechts,senkrecht_links, waagrecht_oben, waagrechts_unten};
     int positionX;
     int positionY;
