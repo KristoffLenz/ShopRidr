13bf96c158dfee2c22be5cf263734834ec7ecf96
Simon Schwärzle <sschwaer@hs-heilbronn.de>
2017-03-14 14:23:36 +0100

    Add images and prefabs

---
+++ Assets/Scipts/TileBuilder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileBuilder : MonoBehaviour {
+
+    public Transform tile;
+    public int beginX;
+    public int y;
+    public int amount;
+
+    public void BuildTiles ()
+    {
+
+        int endX = beginX + amount;
+	    while(beginX < endX)
+	    {
+	        //Instantiate(Resources.Load("Regal"));
+	        Instantiate(tile, new Vector3(0.5F + beginX++, 0.5F + y, 0), Quaternion.identity);
+	    }
+
+	}
+
+}
